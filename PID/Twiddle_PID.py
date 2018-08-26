cddf27073e7b7c21a565349eb94b83b7f8c35101
zhkmxx930 <zhkmxx930@163.com>
2018-08-26 22:58:06 +0800

    add Twiddle PID

--- PID/Twiddle_PID.py
+++ PID/Twiddle_PID.py
@@ -1,17 +1,17 @@
-# -----------
+# ----------------
 # User Instructions
 #
-# Implement a P controller by running 100 iterations
-# of robot motion. The steering angle should be set
-# by the parameter tau so that:
+# Implement twiddle as shown in the previous two videos.
+# Your accumulated error should be very small!
 #
-# steering = -tau_p * CTE - tau_d * diff_CTE - tau_i * int_CTE
+# You don't have to use the exact values as shown in the video
+# play around with different values! This quiz isn't graded just see
+# how low of an error you can get.
 #
-# where the integrated crosstrack error (int_CTE) is
-# the sum of all the previous crosstrack errors.
-# This term works to cancel out steering drift.
+# Try to get your error below 1.0e-10 with as few iterations
+# as possible (too many iterations will cause a timeout).
 #
-# Only modify code at the bottom! Look for the TODO.
+# No cheating!
 # ------------
 
 import random
@@ -105,45 +105,86 @@ class Robot(object):
 #
 # run - does a single control run
 
-# previous P controller
-def run_p(robot, tau, n=100, speed=1.0):
+
+def make_robot():
+    """
+    Resets the robot back to the initial position and drift.
+    You'll want to call this after you call `run`.
+    """
+    robot = Robot()
+    robot.set(0.0, 1.0, 0.0)
+    robot.set_steering_drift(10.0 / 180.0 * np.pi)
+    return robot
+
+
+# NOTE: We use params instead of tau_p, tau_d, tau_i
+def run(robot, params, n=100, speed=1.0):
     x_trajectory = []
     y_trajectory = []
-    for i in range(n):
+    err = 0
+    prev_cte = robot.y
+    int_cte = 0
+    for i in range(2 * n):
         cte = robot.y
-        steer = -tau * cte
+        diff_cte = cte - prev_cte
+        int_cte += cte
+        prev_cte = cte
+        steer = -params[0] * cte - params[1] * diff_cte - params[2] * int_cte
         robot.move(steer, speed)
         x_trajectory.append(robot.x)
         y_trajectory.append(robot.y)
-    return x_trajectory, y_trajectory
+        if i >= n:
+            err += cte ** 2
+    return x_trajectory, y_trajectory, err / n
 
 
-robot = Robot()
-robot.set(0, 1, 0)
+# Make this tolerance bigger if you are timing out!
+def twiddle(tol=0.2):
+    # Don't forget to call `make_robot` before every call of `run`!
+    n_params = 3
+    dp = [1.0 for row in range(n_params)]
+    p = [0.0 for row in range(n_params)]
+
+    robot = make_robot()
+    x_trajectory, y_trajectory, best_err = run(robot, p)
+
+    n = 0
+    # TODO: twiddle loop here
+    while sum(dp) > tol:
+        for i in range(n_params):
+            p[i] += dp[i]
+            robot = make_robot()
+            _, _, err = run(robot, p)
+
+            if err < best_err:
+                best_err = err
+                dp[i] *= 1.1
+
+            else:
+                p[i] -= 2*dp[i]
+                robot = make_robot()
+                _, _, err = run(robot, p)
+
+                if err < best_err:
+                    best_err = err
+                    dp[i] *= 1.1
+                else:
+                    p[i] += dp[i]
+                    dp[i] *= 0.9
+
+        n += 1
+        print('Twiddle # ', n, p, '-->', best_err)
 
 
-def run(robot, tau_p, tau_d, tau_i, n=100, speed=1.0):
-    x_trajectory = []
-    y_trajectory = []
-
-    crosstrack_error = robot.y # cte P
-    integrate_error = 0 # cte integrate
-    # TODO: your code here
-    for i in range(n):
-        diff_crosstrack_error = robot.y - crosstrack_error # delta D
-        crosstrack_error = robot.y
-        integrate_error = integrate_error + crosstrack_error
-        steer = -tau_p * crosstrack_error - tau_d * diff_crosstrack_error - tau_i * integrate_error
-        robot.move(steer, speed)
-        x_trajectory.append(robot.x)
-        y_trajectory.append(robot.y)
-    return x_trajectory, y_trajectory
+    return p, best_err
 
 
-x_trajectory, y_trajectory = run(robot, 0.2, 3.0, 0.004)
+params, err = twiddle(0.0001)
+print("Final twiddle error = {}".format(err))
+robot = make_robot()
+x_trajectory, y_trajectory, err = run(robot, params)
 n = len(x_trajectory)
 
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
-ax1.plot(x_trajectory, y_trajectory, 'g', label='PID controller')
+ax1.plot(x_trajectory, y_trajectory, 'g', label='Twiddle PID controller')
 ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
-plt.show()
